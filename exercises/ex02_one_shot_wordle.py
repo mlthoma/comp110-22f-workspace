1af5f6433cfeb313a2e0849fa8a6d476d9935e33
Maria Thomas <mlthoma@unc.edu>
2022-09-08 16:41:31 -0400

    fix some errors

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -1,11 +1,6 @@
 """EX02 - One Shot Wordle - Another step toward Wordle."""
 __author__ = "730578788"
 
-from ctypes.wintypes import HHOOK
-from operator import indexOf
-from re import A
-
-
 secret_word: str = "python"
 guess: str = str(input("What is your 6-letter guess? "))
 secret_length: int = len(secret_word)
@@ -15,13 +10,10 @@ WHITE_BOX: str = "\U00002B1C"
 GREEN_BOX: str = "\U0001F7E9"
 YELLOW_BOX: str = "\U0001F7E8"
 
-
 emoji_of_guess: str = ""
 
-
-
 while len(guess) != secret_length:
-        guess: str = str(input("That was not " + str(secret_length) + " letters! Try again: "))
+        guess = str(input("That was not " + str(secret_length) + " letters! Try again: "))
 # This loop detects if someone didn't guess a word with the same number of letters as the secret word.
 # This loop prevents people from moving on with the game until they enter a word that has the same length as the secret.
 while i < secret_length:
@@ -43,8 +35,8 @@ while i < secret_length:
             emoji_of_guess = emoji_of_guess + YELLOW_BOX
         else:
             emoji_of_guess = emoji_of_guess + WHITE_BOX
-        #When the letter in the guess doesn't qualify for a green box, this loop checks to see if the letter is located in the word at all
-        #If the letter is in the word, a yellow box is printed in the string. If the letter is not in the word, a white box is printed in the string.
+        # When the letter in the guess doesn't qualify for a green box, this loop checks to see if the letter is located in the word at all
+        # If the letter is in the word, a yellow box is printed in the string. If the letter is not in the word, a white box is printed in the string.
     i += 1
 
 # This part below prints out the final results.    
